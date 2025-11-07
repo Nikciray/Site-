578e5bbbfaec6a651f482c1edf2bd9c00cc71a9e
Belkin <fank.tomphson@gmail.com>
2025-11-07 17:14:57 +0300

    test

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import { Hono } from 'hono'
+import { apply, serve } from '@photonjs/hono'
+ 
+function startServer() {
+  const app = new Hono()
+  apply(app)
+  return serve(app, { port: 3004 })
+}
+ 
+export default startServer()
--- src/renderer/+config.ts
+++ src/renderer/+config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
+import vikePhoton from 'vike-photon/config'
 
 export default {
-  extends: [vikeReact],
+  extends: [vikeReact, vikePhoton],
   ssr: false,
-  reactStrictMode: false
+  reactStrictMode: false,
+  photon: {
+    server: 'server/index.ts'
+  }
 } satisfies Config;
